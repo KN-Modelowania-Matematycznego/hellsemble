76e3d52c13e9cb5f4dbd30e42bf23bf3e2001034
jedrzejrucinski <jedrek.rucinski@gmail.com>
2025-04-07 03:11:51 +0200

    add tabzilla preprocessing for hellsemble testing framework

--- bin/run_experiment.py
+++ bin/run_experiment.py
@@ -51,8 +51,8 @@ def main(
 
 if __name__ == "__main__":
     # Define the directories containing the training and test data.
-    train_dir = "resources/data/aml_sample/train"
-    test_dir = "resources/data/aml_sample/test"
+    train_dir = "resources/data/tabzilla/train"
+    test_dir = "resources/data/tabzilla/test"
 
     # Define the directory to save the results to.
     output_dir = "resources/results/val_greedy"
